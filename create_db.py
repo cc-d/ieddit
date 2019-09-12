89d44021b3d35a368af58d326aafa519ff029599
root <ccarterdev@gmail.com>
2019-09-12 01:43:23 +0000

    building subs next

--- create_db.py
+++ create_db.py
@@ -2,5 +2,10 @@ from flask import Flask, render_template, session, request
 from flask_sqlalchemy import SQLAlchemy
 from ieddit import db
 from models import *
+
 db.create_all()
 db.session.commit()
+
+new_user = User(username='test', email='test@test.com'=,
+	password=generate_password_hash('test'))
+db.session.add(new_user)
--- models.py
+++ models.py
@@ -12,3 +12,8 @@ class User(db.Model):
 
 	def __repr__(self):
 		return '<User %r>' % self.username
+
+class Sub(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(30), unique=True, nullable=False)
+	created_by = db.Column(db.String(20), unique=True, nullable=False)
