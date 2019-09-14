dea7d27911c9ce81904b133e861c9382d5671a9d
root <ccarterdev@gmail.com>
2019-09-14 01:42:15 +0000

    removed console logs

--- static/main.js
+++ static/main.js
@@ -3,15 +3,19 @@ $(document).on('click', '.comment-reply', function () {
 	if ($('#parent_id').val() == replycommid) {
 		return;
 	}
-	//$(this).text('');
+
+	if ($('#comment-reply-box').css('display') != 'hidden') {
+		if ($('#comment-reply-box').parent().children('a')[0] !== undefined) {
+			$($('#comment-reply-box').parent().children('a')[0]).css('display', '');
+		}
+	}
+
 	$('#comment-reply-box').css('display', 'inline-block');
 	$(this).after($('#comment-reply-box'));
-	//$('#comment-reply-box').detach();
+	$(this).css('display', 'none');
+
 	if($('#reply-box').parent().attr('comment_id') !== undefined) {
 		$('#reply-box').parent().text('reply');
-	}/*
-	$('#parent_id').val(replycommid);
-	$('#post_comment_text').val('reply');*/
-    //$(this).append($('#reply-box').html());
+	}
 });
 console.log('loaded');
