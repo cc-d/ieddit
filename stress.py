636d64ce0c09c77a2901d851767e7233da8d0372
root <ccarterdev@gmail.com>
2019-09-16 19:27:16 +0000

    psql migration

--- stress.py
+++
@@ -1,9 +0,0 @@
-import requests
-from datetime import datetime
-import sys
-
-a = datetime.now()
-z = int(sys.argv[1])
-for i in range(z):
-        r = requests.get('http://localhost')
-print(datetime.now() - a)
