c60c061669e5da20f7a8a7ebc1de3dca88c4931f
root <ccarterdev@gmail.com>
2019-09-19 18:21:45 +0000

    login/reg forms

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ DATABASE_URI = 'postgres://test:test@localhost:5432/ieddit'
 SQLALCHEMY_DATABASE_URI = 'postgres://test:test@localhost:5432/ieddit'
 # This will be unique every time create_db.py is ran when testing
 # to force clear sessions
-SECRET_KEY = 'not-a-real-key-|r|7o4YaMWlWi|r|'
+SECRET_KEY = 'not-a-real-key-|r|_uK0xMZAe4|r|'
 URL = 'http://dev.ieddit.com'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
