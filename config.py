4ea7e777a5fea267e90422e9d3324f144f9059da
root <ccarterdev@gmail.com>
2019-09-25 01:56:39 +0000

    last commit included mod/admin user colors

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ DATABASE_URI = 'postgres://test:test@localhost:5432/ieddit'
 SQLALCHEMY_DATABASE_URI = 'postgres://test:test@localhost:5432/ieddit'
 # This will be unique every time create_db.py is ran when testing
 # to force clear sessions
-SECRET_KEY = 'not-a-real-key-|r|WUu0B-ClCY|r|'
+SECRET_KEY = 'not-a-real-key-|r|Y1ylBmQaDY|r|'
 URL = 'http://dev.ieddit.com'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 #SESSION_COOKIE_SAMESITE='Lax'
