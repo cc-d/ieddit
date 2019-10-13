1ec1832b583b18346c5ddf2fe066700de48a04b9
Victor Pascu <victor@whitemantle.com>
2019-10-13 20:36:24 +0300

    Added support for Sentry error monitoring
    Can be enabled/disabled in config.py

--- config.py
+++ config.py
@@ -55,3 +55,7 @@ else:
 	PROXIES = None
 
 PHEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
+
+# Sentry settings. your_dsn should be something like https://hashstring@sentry.io/appno. You can get one by registering at sentry.io
+SENTRY_ENABLED = False
+SENTRY_DSN = 'your_dsn'
