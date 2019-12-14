132c6a5c5ea52a055d111e6a3ed48306ff1db7fa
root <ccarterdev@gmail.com>
2019-12-14 15:39:28 +0000

    fixed a bug where some URLs with uncommon TLDs (ex: .moe) were not get linkified

--- app/functions/functions.py
+++ app/functions/functions.py
@@ -150,8 +150,38 @@ def get_tag_count(text):
 def clean_and_linkify(text):
     tags=['a', 'abbr', 'acronym', 'b', 'code', 'em', 'i', 'li', 'ol', 'strong', 'ul', 'blockquote']
     clean_text = clean(markdown(text), strip=True, tags=tags)
+
+    # horrible hack we shouldn't have to do
+    clean_text, alts = alt_tlds(clean_text)
     clean_text = linkify(clean_text)
+
+    for a in alts:
+        clean_text = clean_text.replace(a[0] + a[1], a[2])
+
+    '''
+    for a in alts.keys():
+        clean_text = clean_text.replace(a, alts[a])
+        clean_text = re.sub('[\s]?^https?:\/\/.*\.' + '.'.join(a.split('.')[:-1]) + '[\s\/]?.*\.*?',
+                            alts[a], clean_text)
+    '''
+
     return clean_text
+
+def alt_tlds(text):
+    '''
+    dirty hack to get .moe support with linkifier
+    ''' 
+    replaced = []
+    tlds = ['.moe']
+    for t in tlds:
+        rep = re.findall('[\s]?^https?:\/\/.*' + t + '[\s\/]?.*\.?', text)
+        for r in rep:
+            ran = rstring(20)
+            replaced.append((ran, '.com', r))
+            text = text.replace(r, ran + '.com')
+
+    return text, replaced
+
 
 
 def pseudo_markup(text, escape_only=False, replace_newlines=True, all_newlines=True):
@@ -244,9 +274,7 @@ def inline_expansion(text):
         repl = re.findall('<a href="([^"]*)" rel="nofollow">([^<]*)', a[0])[0]
         a = (a[0].replace(repl[0], html.escape(repl[0])), a[1])
         a = (a[0].replace(repl[1], html.escape(repl[1])), a[1])
-        print(a)
         z = [x for x in image_exts if a[1].lower()[-5:].find(x) != -1]
-        print(z)
         if (len(z) > 0):
             text = text.replace(a[0], 
                 '<div class="expansion-block">' + a[0] + \
