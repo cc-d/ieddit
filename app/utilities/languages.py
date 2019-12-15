883a5dc698780513d543a05cf7bd3497c5210b9f
root <ccarterdev@gmail.com>
2019-12-15 00:16:20 +0000

    beginning of language support

---
+++ app/utilities/languages.py
@@ -0,0 +1,257 @@
+'''
+essentially a word-for-for dictionary for ieddit
+'''
+LANG = {
+    'action':{
+        'en':'action',
+    },
+    'admin':{
+        'en':'admin',
+    },
+    'add user to mods':{
+        'en':'add user to mods',
+    },
+    'all':{
+        'en':'all',
+    },
+    'all time':{
+        'en':'all time',
+    },
+    'always anonymous':{
+        'en':'always anonymous',
+    },
+    'always post as normal user':{
+        'en':'always post as normal user',
+    },
+    'about':{
+        'en':'about',
+    },
+    'admin':{
+        'en':'admin',
+    },
+    'anonymous':{
+        'en':'anonymous',
+    },
+    'ban':{
+        'en':'ban',
+    },
+    'banned':{
+        'en':'banned',
+    },
+    'blocked':{
+        'en':'blocked',
+    },
+    'block user':{
+        'en':'block user',
+    },
+    'comments':{
+        'en':'comments',
+    },
+    'create post':{
+        'en':'create post',
+    },
+    'change email address':{
+        'en':'change email address',
+    },
+    'change password':{
+        'en':'change password',
+    },
+    'confirm password':{
+        'en':'confirm password',
+    },
+    'day':{
+        'en':'day',
+    },
+    'description':{
+        'en':'description',
+    },
+    'delete':{
+        'en':'delete',
+    },
+    'download user data':{
+        'en':'download user data',
+    },
+    'edit':{
+        'en':'edit',
+    },
+    'enter new password':{
+        'en':'enter new password',
+    },
+    'explore':{
+        'en':'explore',
+    },
+    'joined':{
+        'en':'joined',
+    },
+    'log':{
+        'en':'log',
+    },
+    'logout':{
+        'en':'logout',
+    },
+    'lock':{
+        'en':'lock',
+    },
+    'hide post':{
+        'en':'hide post',
+    },
+    'hide custom sub styles':{
+        'en':'hide custom sub styles',
+    },
+    'information':{
+        'en':'information',
+    },
+    'manage':{
+        'en':'manage',
+    },
+    'mark all posts as nsfw':{
+        'en':'mark all posts as nsfw',
+    },
+    'messages':{
+        'en':'messages',
+    },
+    'mod':{
+        'en':'mod',
+    },
+    'moderator':{
+        'en':'moderator',
+    },
+    'moderator of':{
+        'en':'moderator of',
+    },
+    'mods':{
+        'en':'mods',
+    },
+    'month':{
+        'en':'month',
+    },
+    'next':{
+        'en':'next',
+    },
+    'new':{
+        'en':'new',
+    },
+    'no description':{
+        'en':'no description',
+    },
+    'no banned users':{
+        'en':'no banned users',
+    },
+    'non-mod':{
+        'en':'non-mod',
+    },
+    'nsfw':{
+        'en':'nsfw',
+    },
+    'past':{
+        'en':'past',
+    },
+    'past month':{
+        'en':'past month',
+    },
+    'past hour':{
+        'en':'past hour',
+    },
+    'past day':{
+        'en':'past day',
+    },
+    'past week':{
+        'en':'past week',
+    },
+    'permalink':{
+        'en':'permalink',
+    },
+    'pgp':{
+        'en':'pgp',
+    },
+    'posts':{
+        'en':'posts',
+    },
+    'prev':{
+        'en':'prev',
+    },
+    'preferences':{
+        'en':'preferences',
+    },
+    'posts today':{
+        'en':'posts today',
+    },
+    'recent posts':{
+        'en':'recent posts',
+    },
+    'remote url':{
+        'en':'remote url',
+    },
+    'search':{
+        'en':'search',
+    },
+    'settings':{
+        'en':'settings',
+    },
+    'self-text (if no url)':{
+        'en':'self-text (if no url)',
+    },
+    'self.text':{
+        'en':'self.text',
+    },
+    'self-post text':{
+        'en':'self-post text',
+    },
+    'select':{
+        'en':'select',
+    },
+    'show more':{
+        'en':'show more',
+    },
+    'sticky':{
+        'en':'sticky',
+    },
+    'stickied':{
+        'en':'stickied',
+    },
+    'subscribe':{
+        'en':'subscribe',
+    },
+    'sub':{
+        'en':'sub',
+    },
+    'sub css':{
+        'en':'sub css',
+    },
+    'title':{
+        'en':'title',
+    },
+    'top':{
+        'en':'top',
+    },
+    'unsticky':{
+        'en':'unsticky',
+    },
+    'unlock':{
+        'en':'unlock',
+    },
+    'unban':{
+        'en':'unban',
+    },
+    'unsubscribe':{
+        'en':'unsubscribe',
+    },
+    'update':{
+        'en':'update',
+    },
+    'update description':{
+        'en':'update description',
+    },
+    'update title':{
+        'en':'update title',
+    },
+    'users active':{
+        'en':'users active',
+    },
+    'users':{
+        'en':'users',
+    },
+    'week':{
+        'EN':'week'
+    },
+}
