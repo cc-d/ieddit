3be9949798b6217a5140f7b123359590bcbaaa26
Luis F. Uceta <uzluisf@protonmail.com>
2019-12-21 17:55:29 -0500

    Added Spanish for the multi-language support
    Also removed some "wild" closing curly braces.

--- app/utilities/languages.py
+++ app/utilities/languages.py
@@ -5,549 +5,686 @@ LANG = {
     "account":{
         "en":"account",
         "fr":"compte",
+        "es":"cuenta",
     },
     "action":{
         "en":"action",
         "fr":"action",
+        "es":"acción",
     },
     "admin":{
         "en":"admin",
         "fr":"admin",
+        "es":"admin",
     },
     "add user to mods":{
         "en":"add user to mods",
         "fr":"ajouter l'utilisateur en tant que mods",
+        "es":"agregar el usuario como moderador",
     },
     "all":{
         "en":"all",
         "fr":"Tout",
+        "es":"todo",
     },
     "all time":{
         "en":"all time",
         "fr":"depuis le début",
+        "es":"siempre",
     },
     "always anonymous":{
         "en":"always anonymous",
         "fr":"toujours poster en tant qu'anonyme",
+        "es":"siempre anónimo",
     },
     "always post as normal user":{
         "en":"always post as normal user",
         "fr":"toujours poster en tant qu'utilisateur normal",
+        "es":"siempre publicar como un usuario normal",
     },
     "about":{
         "en":"about",
         "fr":"À propos",
+        "es":"acerca",
     },
     "admin":{
         "en":"admin",
         "fr":"admin",
+        "es":"admin",
     },
     "announcement":{
         "en":"announcement",
         "fr":"announcement",
+        "es":"anuncio",
     },
     "anonymous":{
         "en":"anonymous",
         "fr":"anonyme",
+        "es":"anónimo",
     },
     "ban":{
         "en":"ban",
         "fr":"bannir",
+        "es":"bloquear",
     },
     "banned":{
         "en":"banned",
         "fr":"banni",
+        "es":"bloqueado",
     },
     "block":{
         "en":"block",
         "fr":"bloc",
+        "es":"bloquear",
     },
     "blocked":{
         "en":"blocked",
         "fr":"bloqué",
+        "es":"bloqueado",
     },
     "block user":{
         "en":"block user",
         "fr":"bloquer ce membre",
+        "es":"bloquear usuario",
     },
     "comments":{
         "en":"comments",
         "fr":"commentaires",
+        "es":"comentarios",
     },
     "create":{
         "en":"create",
         "fr":"créer",
+        "es":"crear",
     },
     "create post":{
         "en":"create post",
         "fr":"publier",
+        "es":"publicar",
     },
     "create sub":{
         "en":"create sub",
         "fr":"créer une communauté",
+        "es":"crear una comunidad",
     },
     "change email address":{
         "en":"change email address",
         "fr":"changer mon adresse mail",
+        "es":"cambiar el correo electrónico",
     },
     "change password":{
         "en":"change password",
         "fr":"changer mon mot de passe",
+        "es":"cambiar la contraseña",
     },
     "confirm password":{
         "en":"confirm password",
         "fr":"confirmer le mot de passe",
+        "es":"confirmar la contraseña",
     },
     "confirm new password":{
         "en":"confirm new password",
         "fr":"confirmer le mot de passe",
+        "es":"confirmar la nueva contraseña",
     },
     "create new user":{
         "en":"create new user",
         "fr":"créer un nouvel utilisateur",
+        "es":"crear un nuevo usuario",
     },
     "day":{
         "en":"day",
         "fr":"jour",
+        "es":"día",
     },
     "description":{
         "en":"description",
         "fr":"description",
+        "es":"descripción",
     },
     "delete":{
         "en":"delete",
         "fr":"supprimer",
+        "es":"eliminar",
     },
     "deleted":{
         "en":"deleted",
         "fr":"supprimé",
+        "es":"eliminado",
     },
     "decrypt messages":{
         "en":"decrypt messages",
         "fr":"déchiffrer les messages",
+        "es":"descifrar los mensajes",
     },
     "download user data":{
         "en":"download user data",
         "fr":"télécharger les informations utilisateur",
+        "es":"descargar los datos del usuario",
     },
     "edit":{
         "en":"edit",
         "fr":"éditer",
+        "es":"editar",
     },
     "email address":{
         "en":"email address",
         "fr":"mon adresse mail",
+        "es":"correo electrónico",
     },
     "enter new password":{
         "en":"enter new password",
         "fr":"entrer un nouveau mot de passe",
+        "es":"entrar una nueva contraseña",
     },
-    "enter email to eeset password":{
-        "en":"enter email to eeset password",
+    "enter email to reset password":{
+        "en":"enter email to reset password",
         "fr":"entrez l'e-mail pour réinitialiser le mot de passe",
+        "es":"entrar un correo electrónico para reestablecer la contraseña",
     },
     "explore":{
         "en":"explore",
         "fr":"explorer",
+        "es":"explorar",
     },
     "forgot password":{
         "en":"forgot password",
         "fr":"mot de passe oublié",
+        "es":"olvidaste la contraseña",
     },
     "future content":{
         "en":"future content",
         "fr":"contenu futur",
+        "es":"contenido futuro",
     },
     "joined":{
         "en":"joined",
         "fr":"inscrit le ",
+        "es":"se unió",
     },
     "log":{
         "en":"log",
         "fr":"bûche",
+        "es":"registro",
     },
     "login":{
         "en":"login",
         "fr":"se connecter",
+        "es":"iniciar sesión",
     },
     "login to comment":{
         "en":"login to comment",
         "fr":"connectez-vous pour commenter",
+        "es":"iniciar sesión para comentar",
     },
     "logout":{
         "en":"logout",
         "fr":"se déconnecter",
+        "es":"cerrar sesión",
     },
     "lock":{
         "en":"lock",
         "fr":"verouiller",
+        "es":"acerrojar",
     },
     "lost passwords cannot be recovered without an email":{
         "en":"Lost passwords cannot be recovered without an email.",
         "fr":"Les mots de passe perdus ne peuvent pas être récupérés sans e-mail",
+        "es":"Las contraseñas perdidas no pueden recuperarse sin un correo electrónico",
     },
     "hide comment":{
         "en":"hide comment",
         "fr":"masquer le commentaire",
+        "es":"ocultar el comentario",
     },
     "hide post":{
         "en":"hide post",
         "fr":"masquer la publication",
+        "es":"ocultar la publicación",
     },
     "hide custom sub styles":{
         "en":"hide custom sub styles",
         "fr":"masquer les styles personnalisés pour les communautés",
+        "es":"ocultar los estilos personalizados de la comunidad",
     },
     "hide custom sub language":{
         "en":"hide custom sub language",
         "fr":"masquer la langue de la communauté",
+        "es":"ocultar los lenguajes de la comunidad",
     },
     "hot":{
         "en":"hot",
         "fr":"chaud",
+        "es":"activo",
     },
     "information":{
         "en":"information",
         "fr":"information",
+        "es":"información",
     },
     "manage":{
         "en":"manage",
         "fr":"gérer",
+        "es":"manejar",
     },
     "mark all posts nsfw":{
         "en":"mark all posts nsfw",
         "fr":"Marquer toutes les publications comme explicites",
+        "es":"marcar todas la publicaciones como explícitas",
     },
     "message encrypted":{
         "en":"message encrypted",
         "fr":"message crypté",
+        "es":"mensaje cifrado",
     },
     "messages":{
         "en":"messages",
         "fr":"messages",
+        "es":"mensajes",
     },
     "mod":{
         "en":"mod",
         "fr":"mod",
+        "es":"mod",
     },
     "moderator":{
         "en":"moderator",
         "fr":"modérateur",
+        "es":"moderador",
     },
     "moderator of":{
         "en":"moderator of",
         "fr":"modérateur de",
+        "es":"moderador de",
     },
     "moderators":{
         "en":"moderators",
         "fr":"modérateurs",
+        "es":"moderadores",
     },
     "mods":{
         "en":"mods",
         "fr":"mods",
+        "es":"mods",
     },
     "month":{
         "en":"month",
         "fr":"mois",
+        "es":"mes",
     },
     "next":{
         "en":"next",
         "fr":"suivant",
+        "es":"siguiente",
     },
     "new":{
         "en":"new",
         "fr":"nouveau",
+        "es":"nuevo",
     },
     "new password":{
         "en":"new password",
         "fr":"un nouveau mot de passe",
+        "es":"nueva contraseña",
     },
     "new username":{
         "en":"new username",
         "fr":"nouveau nom d'utilisateur",
+        "es":"nuevo nombre de usuario",
     },
     "no description":{
         "en":"no description",
         "fr":"pas de description",
+        "es":"no descripción",
     },
     "no banned users":{
         "en":"no banned users",
         "fr":"pas d'utilisateurs bannis",
+        "es":"no usuarios bloqueados",
     },
     "no comments":{
         "en":"no comments",
         "fr":"sans commentaires",
+        "es":"no comentarios",
     },
     "no posts":{
         "en":"no posts",
         "fr":"sans publications",
+        "es":"no publicaciones",
     },
     "no title":{
         "en":"no title",
         "fr":"pas de titre",
+        "es":"no título",
     },
     "non-mod":{
         "en":"non-mod",
         "fr":"non-mod",
+        "es":"non-mod",
     },
     "no messages":{
         "en":"no messages",
         "fr":"pas de messages",
+        "es":"no mensajes",
     },
     "no more subs to show":{
         "en":"no more subs to show",
         "fr":"plus de communautés à montrer",
+        "es":"no más comunidadades que mostrar",
     },
     "nsfw":{
         "en":"nsfw",
         "fr":"contenu explicite",
+        "es":"contenido explícito",
     },
     "optional":{
         "en":"optional",
         "fr":"optionnel",
+        "es":"opcional",
     },
     "parent":{
         "en":"parent",
         "fr":"parente",
+        "es":"padre",
     },
     "past":{
         "en":"past",
         "fr":"période",
+        "es":"pasado",
     },
     "past month":{
         "en":"past month",
         "fr":"ce mois-ci",
+        "es":"mes pasado",
     },
     "past hour":{
         "en":"past hour",
         "fr":"cette heure-ci",
+        "es":"hora pasado",
     },
     "past day":{
         "en":"past day",
         "fr":"ce jour-ci",
+        "es":"día pasado",
     },
     "past week":{
         "en":"past week",
         "fr":"cette semaine-ci",
+        "es":"semana pasada",
     },
     "password":{
         "en":"password",
         "fr":"mot de passe",
+        "es":"contraseña",
     },
     "permalink":{
         "en":"permalink",
         "fr":"perma-lien",
+        "es":"enlace permanente",
     },
     "pgp":{
         "en":"pgp",
         "fr":"adresse pgp",
+        "es":"dirección pgp",
     },
     "post is locked":{
         "en":"post is locked",
         "fr":"le message est verrouillé",
+        "es":"la publicación está acerrojada",
     },
     "posts":{
         "en":"posts",
         "fr":"publications",
+        "es":"publicaciones",
     },
     "prev":{
         "en":"prev",
         "fr":"précédent",
+        "es":"previo",
     },
     "preferences":{
         "en":"preferences",
         "fr":"préférences",
+        "es":"preferencias",
     },
     "posts today":{
         "en":"posts today",
         "fr":"publiés aujourd'hui",
+        "es":"publicaciones hoy",
     },
     "recent posts":{
         "en":"recent posts",
         "fr":"vos publications récentes",
+        "es":"publicaciones recientes",
     },
     "reply":{
         "en":"reply",
         "fr":"répondre",
+        "es":"respuesta",
     },
     "remote url":{
         "en":"remote url",
         "fr":"adresse URL",
+        "es":"URL remota",
     },
     "removed by":{
         "en":"removed by",
         "fr":"supprimé par",
+        "es":"removido por",
     },
     "save preferences":{
         "en":"save preferences",
         "fr":"einstellungen speichern",
+        "es":"guardar preferencias",
     },
     "search":{
         "en":"search",
         "fr":"recherche",
+        "es":"buscar",
     },
     "settings":{
         "en":"settings",
         "fr":"oréférences",
+        "es":"configuraciones",
     },
     "self-text (if no url)":{
         "en":"self-text (if no url)",
         "fr":"texte (s'il ne s'agit pas d'une URL)",
+        "es":"texto (si no hay url)",
     },
     "self.text":{
         "en":"self.text",
         "fr":"texte",
+        "es":"texto",
     },
     "self-post text":{
         "en":"self-post text",
         "fr":"texte de publication",
+        "es":"publicación de texto",
     },
     "select":{
         "en":"select",
         "fr":"sélectionner",
+        "es":"seleccionar",
     },
     "send email":{
         "en":"send email",
         "fr":"envoyer un e-mail",
+        "es":"enviar un correo electrónico",
     },
     "sent":{
         "en":"sent",
         "fr":"sent",
+        "es":"enviado",
     },
     "sent to":{
         "en":"sent to",
         "fr":"envoyé à",
+        "es":"enviar a",
     },
     "show all":{
         "en":"show all",
         "fr":"montrer tout",
+        "es":"mostrar todos",
     },
     "show more":{
         "en":"show more",
         "fr":"montrer davantage",
+        "es":"mostrar más",
     },
     "stats for":{
         "en":"stats for",
         "fr":"stats pour",
+        "es":"stats para",
     },
     "sticky":{
         "en":"sticky",
         "fr":"mettre en avant",
+        "es":"fijar",
     },
     "stickied":{
         "en":"stickied",
         "fr":"À été mit en avant",
+        "es":"fijado",
     },
     "subscribe":{
         "en":"subscribe",
         "fr":"souscrire",
+        "es":"suscribirse",
     },
     "sub":{
         "en":"sub",
         "fr":"communauté",
+        "es":"comunidad",
     },
     "sub css":{
         "en":"sub css",
         "fr":"code CSS",
+        "es":"css de la comunidad",
     },
     "sub name":{
         "en":"sub name",
         "fr":"nom de la communauté",
+        "es":"nombre de la comunidad",
     },
     "sub title":{
         "en":"sub title",
         "fr":"titre de communauté",
+        "es":"título de la comunidad",
     },
     "title":{
         "en":"title",
         "fr":"titre",
+        "es":"título",
     },
     "top":{
         "en":"top",
         "fr":"populaire",
+        "es":"popular",
     },
     "this sub has no description":{
         "en":"this sub has no description",
         "fr":"ce sous n'a pas de description",
+        "es":"esta comunidad no tiene descripción",
     },
     "type":{
         "en":"type",
         "fr":"type",
+        "es":"tipo",
     },
     "unsticky":{
         "en":"unsticky",
         "fr":"enlever la mise en avant",
+        "es":"despegar",
     },
     "unblock":{
         "en":"unblock",
         "fr":"débloque",
+        "es":"desbloquear",
     },
     "unlock":{
         "en":"unlock",
         "fr":"débloquer",
+        "es":"quitar cerrojo",
     },
     "unban":{
         "en":"unban",
         "fr":"débannir",
+        "es":"desbloquear",
     },
     "unsubscribe":{
         "en":"unsubscribe",
         "fr":"se désinscrire",
+        "es":"cancelar suscripción",
     },
     "update":{
         "en":"update",
         "fr":"mettre à jour",
+        "es":"actualizar",
     },
     "update description":{
         "en":"update description",
         "fr":"mettre à jour la description",
+        "es":"actualizar la descripción",
     },
     "update title":{
         "en":"update title",
         "fr":"mettre à jour le titre",
+        "es":"actualizar el título",
     },
     "user":{
         "en":"user",
         "fr":"utilisateur",
+        "es":"usuario",
     },
     "users active":{
         "en":"users active",
         "fr":"utilisateurs actifs",
+        "es":"usuarios activos",
     },
     "users":{
         "en":"users",
         "fr":"utilisateurs",
+        "es":"usuarios",
     },
     "username":{
         "en":"username",
         "fr":"nom d'utilisateur",
+        "es":"nombre de usuario",
     },
     "view all comments":{
         "en":"view all comments",
         "fr":"voir tous les commentaires",
+        "es":"ver todos los comentarios",
     },
     "view more":{
         "en":"view more",
         "fr":"afficher plus",
+        "es":"ver más",
     },
     "week":{
         "en":"week",
         "fr":"semaine",
+        "es":"semana",
     },
     "you are":{
         "en":"you are",
         "fr":"tu es",
+        "es":"tú eres",
     },
     "you are viewing a single comment":{
         "en":"you are viewing a single comment",
         "fr":"vous regardez un seul commentaire",
+        "es":"tú estás viendo un solo comentario",
     },
     "you are already logged in as":{
         "en":"you are already logged in as",
         "fr":"vous êtes déjà connecté en tant que",
+        "es":"tú has iniciado sesión como",
     },
     "you have a encrypted messages but (probably) cannot read it":{
         "en":"you have a encrypted messages but (probably) cannot read it",
         "fr":"vous avez un message crypté mais (probablement) ne pouvez pas le lire",
+        "es":"tú has enviado un mensaje cifrado pero (probablemente) no puede leerse",
     }
 }
